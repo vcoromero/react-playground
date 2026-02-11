fc7a22f477d5eff27790a9958e2ec0c1810e99ae
Victor Romero <vcoromero@gmail.com>
2026-02-11 15:58:26 -0600

    feat: reading vars

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
 export function MyAwesomeApp() {
+    const firstName = 'Victor';
+    const lastName = 'Romero';
+
+    const myFavoriteGames = ['Halo 3', 'Gears 5', 'Hollow Knight', 'Silksong'];
+    const isActive = true;
+
+    const myAddress = {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'CA',
+        zip: '12345'
+    };
+
     return (
         <>
-            <h1>Victor</h1>
-            <h3>Romero</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{myFavoriteGames.join(', ')}</p>
+            <p>{2 + 2}</p>
+            <p>{isActive ? 'Active' : 'Inactive'}</p>
+
+            <p>{JSON.stringify(myAddress)}</p>
         </>
     )
 }

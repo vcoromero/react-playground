a0e7ca66e8606ce65ed2c52d7071f7a63504d6ef
Victor Romero <vcoromero@gmail.com>
2026-02-17 00:38:48 -0600

    feat: adding cache for gifs

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import type { Gif } from '../interfaces/gif.interface'
 import { getGifsByQuery } from '../actions/get-gifs-by-query.actions'
 
+const gifsCache: Record<string, Gif[]> = {}
+
 export const useGifs = () => {
     const [gifs, setGifs] = useState<Gif[]>([])
     const [previousTerms, setPreviousTerms] = useState<string[]>([])
 
+
     const handleTermClicked = async (term: string) => {
+        if (gifsCache[term]) {
+            setGifs(gifsCache[term])
+            return;
+        }
         const gifs = await getGifsByQuery(term)
         setGifs(gifs)
     }
@@ -21,6 +28,8 @@ export const useGifs = () => {
 
         const gifs = await getGifsByQuery(query)
         setGifs(gifs)
+
+        gifsCache[query] = gifs
     }
 
     return {
